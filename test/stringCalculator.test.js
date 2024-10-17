7bc3ff6a9ba48343590cd62930933ff0eaee9399
Varun Rawat <varunrawat9410@gmail.com>
2024-10-17 20:52:25 +0530

    wrote code to pass first test case

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,8 +1,10 @@
+const StringCalculator = require('../stringCalculator')
+
 describe("String Calculator", function() {
     let calculator
 
     beforeEach(function() {
-       
+        calculator = new StringCalculator()
     })
 
     test("should return zero when '' is passed", function() {
