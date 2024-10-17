f4ee7948ef0dcd0de07660078c463c38125d88a0
Varun Rawat <varunrawat9410@gmail.com>
2024-10-17 20:48:42 +0530

    Wrote first failed test case

---
+++ test/stringCalculator.test.js
@@ -0,0 +1,11 @@
+describe("String Calculator", function() {
+    let calculator
+
+    beforeEach(function() {
+       
+    })
+
+    test("should return zero when '' is passed", function() {
+        expect(calculator.add('')).toBe(0)
+    })
+})
