e19345b4fa01836d1d1ee06ea3a5867834445518
Varun Rawat <varunrawat9410@gmail.com>
2024-10-18 09:36:22 +0530

    modified function to handle test cases

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,7 +3,20 @@ class StringCalculator {
         if (numbers === '') {
             return 0
         }
+        let numArray = numbers.split(',')
+        if(numArray.length===1) {
+            return parseInt(numArray[0])
+        }
+
+        if(numArray.length>1) {
+            let sum = 0
+            for(let i=0;i<numArray.length;i++) {
+                sum+=parseInt(numArray[i])
+            }
+            return sum
+        }
     }
+
 }
 
 module.exports = StringCalculator
